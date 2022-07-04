988316c361bcbef2c92e36ee21ecacd8ca2bc150
duc.bui.trung <duc.bui.trung@mailofatechjsc.com>
2022-07-04 17:01:01 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,8 @@ use App\Http\Controllers\AdminController;
 
 
 Route::get('/admin/dashboard',[AdminController::class, 'dashboard']);
+
+Route::get('/',[HomeController::class,'index']);
 
 
 
